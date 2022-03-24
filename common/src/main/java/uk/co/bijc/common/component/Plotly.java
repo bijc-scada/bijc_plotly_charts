0c027ddcff9070a3129672a536b019f81f9c4543
Lewis Storey <lewis.storey@bijc.co.uk>
2022-03-24 14:28:28 +0000

    Added variants to palette

--- common/src/main/java/uk/co/bijc/common/component/Plotly.java
+++ common/src/main/java/uk/co/bijc/common/component/Plotly.java
@@ -25,8 +25,8 @@ public class Plotly {
     public static JsonSchema SCHEMA = JsonSchema
             .parse(PlotlyComponents.class.getResourceAsStream("/plotly.props.json"));
 
-    static JsonObject getVariant(String fileName) {
-        return (new JsonParser()).parse(new InputStreamReader(PlotlyComponents.class.getResourceAsStream(fileName))).getAsJsonObject();
+    static JsonObject getVariant(String chartName) {
+        return (new JsonParser()).parse(new InputStreamReader(PlotlyComponents.class.getResourceAsStream(String.format("/variants/plotly.%s.props.json",chartName)))).getAsJsonObject();
     }
 
     /**
@@ -44,8 +44,23 @@ public class Plotly {
             .setName("Plotly")
             .setIcon(new ImageIcon(PlotlyComponents.class.getResource("/size-icon.png")))
             .addPaletteEntry("", "Plotly", "A Plotly.js chart component", null, null)
-            .addPaletteEntry("plotly-bar", "Plotly Bar", "A Plotly.js bar component", null, getVariant("/variants/plotly.bar.props.json"))
-            .addPaletteEntry("plotly-line", "Plotly Line", "A Plotly.js line component", null, getVariant("/variants/plotly.line.props.json"))
+            .addPaletteEntry("plotly-bar", "Plotly Bar", "A Plotly.js bar component", null, getVariant("bar"))
+            .addPaletteEntry("plotly-line", "Plotly Line", "A Plotly.js line component", null, getVariant("line"))
+            .addPaletteEntry("plotly-area", "Plotly Area", "A Plotly.js area component", null, getVariant("area"))
+            .addPaletteEntry("plotly-boxplot", "Plotly Boxplot", "A Plotly.js boxplot component", null, getVariant("boxplot"))
+            .addPaletteEntry("plotly-bubble", "Plotly Bubble", "A Plotly.js bubble component", null, getVariant("bubble"))
+            .addPaletteEntry("plotly-sunburst", "Plotly Sunburst", "A Plotly.js sunburst component", null, getVariant("sunburst"))
+            .addPaletteEntry("plotly-treemap", "Plotly Treemap", "A Plotly.js treemap component", null, getVariant("treemap"))
+            .addPaletteEntry("plotly-contour", "Plotly Contour", "A Plotly.js contour component", null, getVariant("contour"))
+            .addPaletteEntry("plotly-heatmap", "Plotly Heatmap", "A Plotly.js heatmap component", null, getVariant("heatmap"))
+            .addPaletteEntry("plotly-radar", "Plotly Radar", "A Plotly.js radar component", null, getVariant("radar"))
+            .addPaletteEntry("plotly-waterfall", "Plotly Waterfall", "A Plotly.js waterfall component", null, getVariant("waterfall"))
+            .addPaletteEntry("plotly-gauge", "Plotly Gauge", "A Plotly.js gauge component", null, getVariant("gauge"))
+            .addPaletteEntry("plotly-3dsurface", "Plotly 3D Surface", "A Plotly.js 3D surface component", null, getVariant("3dsurface"))
+            .addPaletteEntry("plotly-scatter3d", "Plotly Scatter 3D", "A Plotly.js scatter 3D component", null, getVariant("scatter3d"))
+            .addPaletteEntry("plotly-streamtube", "Plotly Streamtube", "A Plotly.js streamtube component", null, getVariant("streamtube"))
+            .addPaletteEntry("plotly-isosurface", "Plotly Isosurface", "A Plotly.js isosurface component", null, getVariant("isosurface"))
+            .addPaletteEntry("plotly-ribbon", "Plotly Ribbon", "A Plotly.js ribbon component", null, getVariant("ribbon"))
             // getVariantProps("/variants/bijcplotly.bar.props.json"))
             .setDefaultMetaName("plotly")
             .setResources(PlotlyComponents.BROWSER_RESOURCES)
